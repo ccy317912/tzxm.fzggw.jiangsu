25a709064746e20559f544e21c4338f48fdd798e
chencongying <18768427422@163.com>
2025-02-27 16:14:30 +0800

    fix:初始化

---
+++ dataset.js
@@ -0,0 +1,21 @@
+/**
+ * @description 数据集
+ * @project_name 项目名称
+ * @project_code 项目编号
+ * @filing_code 备案编号
+ * @result 备案结果
+ * @filing_date 备案日期
+ * @filing_authority 备案机构
+ * @scheduled_date 预计完成日期
+ */
+const dataset_apply_pro = {
+  "2412-320621-89-01-890486": {
+    project_name: "江苏阳昭新能源科技有限公司一-邰友华98.425KW屋顶户用光伏项目",
+    project_code: "2412-320621-89-01-890486",
+    filing_code: "海安数据备〔2024〕 261号",
+    result: "1",
+    filing_date: "2024/12/15",
+    filing_authority: "海安市数据局",
+    scheduled_date: "2024",
+  },
+};
